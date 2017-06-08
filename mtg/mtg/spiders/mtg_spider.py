789e54072acac2f6adfe2c9468112d3c60e5665b
stberg3 <stberg3@gmail.com>
2017-06-08 16:36:41 -0500

    fixed bugs

--- mtg/mtg/spiders/mtg_spider.py
+++ mtg/mtg/spiders/mtg_spider.py
@@ -1,5 +1,6 @@
 import scrapy
 import yaml
+import re
 
 image_base_url = "http://magiccards.info/scans/"
 card_base_url = "http://magiccards.info"
@@ -11,7 +12,7 @@ resources = yaml.load(open("resources.yaml", "r"))
 class QuotesSpider(scrapy.Spider):
     name = "mtg"
 
-    start_urls = yaml.load(open("set_urls.yaml", "r"))[:10]
+    start_urls = yaml.load(open("set_urls.yaml", "r"))
 
     def parse(self, response):
         log = open("log.log", "w")
@@ -25,7 +26,24 @@ class QuotesSpider(scrapy.Spider):
 
                 entry['number'] = row.xpath("./td/text()")[0].extract()
                 entry['name'] = row.xpath("./td/a/text()").extract_first()
+                entry['card_url'] = response.urljoin(
+                    row.xpath("./td/a/@href").extract_first())
+
+                try:
+                    set_url_part, lang_url_part, card_url_part = \
+                        re.search(r'(\w+)/(\w+)/(\w+)\.html',
+                            entry['card_url']).groups()
+                except AttributeError as ae:
+                    self.log("\nBad url? \t{}\n".format(entry['card_url']))
+
+                image_suffix = "{}/{}/{}".format(
+                    lang_url_part, set_url_part, card_url_part)
+
+                entry["image_url"] = 'http://magiccards.info/scans/' + \
+                                     image_suffix + ".jpg"
+
                 entry['type'] = row.xpath("./td/text()")[1].extract()
+
 
                 # Some entries have no mana cost (lands), so this checks
                 # the number of td tags that contain text
@@ -33,17 +51,16 @@ class QuotesSpider(scrapy.Spider):
                     entry['mana'] = row.xpath("./td/text()")[2].extract()
                     entry['rarity'] = row.xpath("./td/text()")[3].extract()
                     entry['artist'] = row.xpath("./td/text()")[4].extract()
-                    entry['edition'] = row.xpath("./td/text()")[5].extract()
+                    entry['edition'] = row.xpath("./td/text()")[5].extract()[1:]
                 else:
                     entry['mana'] = None
                     entry['rarity'] = row.xpath("./td/text()")[2].extract()
                     entry['artist'] = row.xpath("./td/text()")[3].extract()
-                    entry['edition'] = row.xpath("./td/text()")[4].extract()
+                    entry['edition'] = row.xpath("./td/text()")[4].extract()[1:]
 
                 entry['card_url'] = card_base_url + \
                                     row.xpath("./td[2]/a/@href").extract_first()
-                entry['image_url'] = image_base_url + \
-                                    row.xpath("./td[2]/a/@href").extract_first()
+
 
             except IndexError as ie:
                 log.write("\n\nERROR:"+ response.url +
