5d7b2f724eb9ad8055475c9bfd61c305967fe11b
stberg3 <stberg3@gmail.com>
2017-07-07 09:44:27 -0500

    More elegant directory checking

--- image_harvester.py
+++ image_harvester.py
@@ -7,6 +7,24 @@ from PIL import Image
 from hashlib import sha1
 import urllib, os, sys, html, time
 
+class DownloadThread(threading.Thread):
+    def __init__(self, url, store_directory):
+        threading.Thread.__init__(self)
+        self.url = url
+        self.store_directory = store_directory
+
+        def run(self):
+            fname = str(self.store_directory+"/"+
+            (sha1(self.url.encode("utf8")).hexdigest()) + ".jpg")
+
+            tmp_name = urllib.request.urlretrieve(self.url)[0]
+            im = Image.open(tmp_name)
+
+            try:
+                im.save(fname, "JPEG")
+            except OSError:
+                pass
+
 store_directory = "images"
 driver = webdriver.Firefox()
 query = html.escape(sys.argv[1]).replace(" ", "+").replace(";", "").replace("&#x", "%")
@@ -17,10 +35,8 @@ if(len(sys.argv)==2):
 else:
     driver.get("https://www.google.com/search?site=&tbm=isch&source=hp&biw=1855&bih=966&q=cat&oq=cat&gs_l=img.3..0l10.1451.2486.0.2613.8.6.2.0.0.0.67.253.5.5.0....0...1.1.64.img..1.7.253.0.Xy1TKhU0Sz0")
 
-try:
+if not os.dir.isdir(store_directory):
     os.mkdir(store_directory)
-except FileExistsError:
-    pass
 
 for i in range(2):
     for i in range(4):
@@ -30,7 +46,7 @@ for i in range(2):
         # to appear on the page
         time.sleep(1)
     try:
-        # Click the button taht make more results appear
+        # Click the button that makes more results appear
         driver.find_element_by_id("smb").click()
     except ElementNotInteractableException:
         pass
@@ -38,22 +54,6 @@ for i in range(2):
 img_urls = [elem.get_attribute("src") for elem in
                 driver.find_elements_by_css_selector("img.rg_ic")]
 
-class DownloadThread(threading.Thread):
-    def __init__(self, url, store_directory):
-        threading.Thread.__init__(self)
-        self.url = url
-        self.store_directory = store_directory
-
-    def run(self):
-        fname = str(self.store_directory+"/"+(sha1(self.url.encode("utf8")).hexdigest())+
-                                               ".jpg")
-        tmp_name = urllib.request.urlretrieve(self.url)[0]
-        im = Image.open(tmp_name)
-
-        try:
-            im.save(fname, "JPEG")
-        except OSError:
-            pass
 
 threads = []
 with open("threading.log", "w") as file:
